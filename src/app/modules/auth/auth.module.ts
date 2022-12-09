c5fc40b7fe77a7a5ff76f850448b5898d98f7b4a
mechztcc <dojathan@gmail.com>
2022-12-09 10:16:00 -0300

    feat: added ng-icons with octicons

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { NgIconsModule } from '@ng-icons/core';
+import { octPerson } from '@ng-icons/octicons';
+
 import { AuthRoutingModule } from './auth-routing.module';
 import { CreateAccountPageComponent } from './pages/create-account-page/create-account-page.component';
 
 @NgModule({
   declarations: [CreateAccountPageComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+
+    NgIconsModule.withIcons({ octPerson }),
+  ],
 })
 export class AuthModule {}
--- src/app/modules/auth/pages/create-account-page/create-account-page.component.ts
+++ src/app/modules/auth/pages/create-account-page/create-account-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-create-account-page',
   templateUrl: './create-account-page.component.html',
