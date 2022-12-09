5591b4a04a7093d66d8c36b21b7e8dba12b4f20e
mechztcc <dojathan@gmail.com>
2022-12-09 10:33:50 -0300

    feat: created form to sign in

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { NgIconsModule } from '@ng-icons/core';
-import { octPerson } from '@ng-icons/octicons';
+import { octPerson, octMention, octEye, octEyeClosed } from '@ng-icons/octicons';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { CreateAccountPageComponent } from './pages/create-account-page/create-account-page.component';
@@ -13,7 +13,7 @@ import { CreateAccountPageComponent } from './pages/create-account-page/create-a
     CommonModule,
     AuthRoutingModule,
 
-    NgIconsModule.withIcons({ octPerson }),
+    NgIconsModule.withIcons({ octPerson, octMention, octEye, octEyeClosed }),
   ],
 })
 export class AuthModule {}
