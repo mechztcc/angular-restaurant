e040f2303fa8186bd9dc8bdbbe7154d7d0ac7829
mechztcc <dojathan@gmail.com>
2022-12-13 10:07:22 -0300

    feat: created restaurant module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,14 +4,17 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
+import { octArchive } from '@ng-icons/octicons';
+import { NgIconsModule } from '@ng-icons/core';
 
 @NgModule({
-  declarations: [
-    HomePageComponent
-  ],
+  declarations: [HomePageComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    NgIconsModule.withIcons({
+      octArchive,
+    }),
+  ],
 })
-export class HomeModule { }
+export class HomeModule {}
